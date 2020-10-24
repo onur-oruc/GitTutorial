462350eb3cce425635d53f1f924fc99a1fadfaca
Onur Oruç <onuroruc06@gmail.com>
2020-10-24 10:51:48 +0300

    master initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main( String args[]) {
+
+        System.out.println("selam adem");
+        System.out.println("selam bebeq");
+        System.out.println("selam adem");
+
+    }
+}
